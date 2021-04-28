f9fcd2863e0dec8653cd6aa14684c0b693c98cc4
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-04-28 21:34:23 +0200

    add styles to the Home component using styled-components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,22 @@
+import styled from 'styled-components';
+
+const HomeContainer = styled.main`
+        display: flex;
+        justify-content: center;
+        margin: 35px auto;
+        padding: 40px 0;
+        max-width: 70%;
+        text-align: center;
+        border-radius: 5px;
+        box-shadow: 0 0 30px black;
+        background-color: white;
+    `
+
 const Home = () => {
     return ( 
-        null
+        <HomeContainer>
+            <h1>Witaj w Naszym Katalogu Książek!</h1>
+        </HomeContainer>
      );
 }
  
