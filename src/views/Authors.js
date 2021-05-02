f9d0417d7f65efd7e3844c1808d6131ea754b37b
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-02 19:35:13 +0200

    move the fetch of the authors data to the inside of the useEffect hook and set a dependancy array in this hook

--- src/views/Authors.js
+++ src/views/Authors.js
@@ -8,22 +8,21 @@ const Authors = () => {
     const [state, dispatch] = useContext(AuthorsContext);
     const url = 'http://139.162.147.107:3493/authors';
 
-    const fetchAuthors = async () => {
-        try {
-            let response = await fetch(url);
-            response = await response.json();
-            console.log(response);
-            const authorsData = Object.keys(response).map(key => ({id: key, ...response[key]}));
-            console.log(authorsData);
-            dispatch({ type: 'SET_AUTHORS', payload: authorsData})
-        } catch (error) {
-            dispatch({ type: 'SET_ERROR', payload: error });
-        }
-    }
-
     useEffect(() => {
+        const fetchAuthors = async () => {
+            try {
+                let response = await fetch(url);
+                response = await response.json();
+                const authorsData = Object.keys(response).map(key => ({id: key, ...response[key]}));
+                dispatch({ type: 'SET_AUTHORS', payload: authorsData})
+            } catch (error) {
+                dispatch({ type: 'SET_ERROR', payload: error });
+            }
+        }
+
         fetchAuthors()
-    }, []);
+    }, [dispatch]);
+
 
     return ( 
         <Container>
