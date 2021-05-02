ec152a47db8a1a543846ead9fc089e653526ab0d
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-02 19:15:10 +0200

    use useContext hook to connect to the global state; fetch the data and reformat them to an array using Object.keys and map method, log the results into the console; use dispatch methods to get the state of the data

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.main`
         display: flex;
+        flex-direction: column;
         justify-content: center;
         margin: 55px auto;
         padding: 40px 0;
--- src/views/Authors.js
+++ src/views/Authors.js
@@ -1,7 +1,35 @@
+import { useContext } from 'react';
+import { useEffect } from 'react';
+import { Container } from '../components/Home';
+import { AuthorsContext } from '../store/AuthorsStore';
+
+
 const Authors = () => {
+    const [state, dispatch] = useContext(AuthorsContext);
+    const url = 'http://139.162.147.107:3493/authors';
+
+    const fetchAuthors = async () => {
+        try {
+            let response = await fetch(url);
+            response = await response.json();
+            console.log(response);
+            const authorsData = Object.keys(response).map(key => ({id: key, ...response[key]}));
+            console.log(authorsData);
+            dispatch({ type: 'SET_AUTHORS', payload: authorsData})
+        } catch (error) {
+            dispatch({ type: 'SET_ERROR', payload: error });
+        }
+    }
+
+    useEffect(() => {
+        fetchAuthors()
+    }, []);
+
     return ( 
-        null
-     );
+        <Container>
+            <h2>Autorzy</h2>
+        </Container>
+    );
 }
  
 export default Authors;
