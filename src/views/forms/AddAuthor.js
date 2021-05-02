43cf13b3a94543ec8bad9f50b18cf0c3e685f29e
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-02 22:52:34 +0200

    add the handleSubmit function on the form that includes making a post request to the authors endpoint

--- src/views/forms/AddAuthor.js
+++ src/views/forms/AddAuthor.js
@@ -1,16 +1,62 @@
+import { useContext, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { AuthorsContext } from '../../store/AuthorsStore';
+
 import { Container } from "../../components/Home";
 import { Form, Label, Input } from '../../styled-components/styled-form';
 import { AddButton } from '../../styled-components/styled-table';
 
 const AddAuthor = () => {
+    const history = useHistory();
+    const [lastName, setLastName] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [state, dispatch] = useContext(AuthorsContext);
+    const url = 'http://139.162.147.107:3493/authors';
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        let newAuthor = {
+            lastName,
+            firstName
+        };
+
+        fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newAuthor)
+        }).then(() => {
+            console.log('New author added');
+            dispatch({ type: 'ADD_AUTHOR', payload: newAuthor});
+            history.push('/autorzy');
+
+        })
+
+        setLastName('');
+        setFirstName('');
+    };
+
+
     return ( 
         <Container>
             <h2>Dodaj Nowego Autora</h2>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Label htmlFor="lastName">Nazwisko:</Label>
-                <Input type="text" placeholder="Wpisz nazwisko autora" required />
+                <Input 
+                    type="text" 
+                    value={lastName}
+                    onChange={event => setLastName(event.target.value)}
+                    placeholder="Wpisz nazwisko autora" 
+                    required 
+                />
                 <Label htmlFor="firstName">Imię:</Label>
-                <Input type="text" placeholder="Wpisz imię autora" required />
+                <Input 
+                    type="text" 
+                    value={firstName}
+                    onChange={event => setFirstName(event.target.value)}
+                    placeholder="Wpisz imię autora" 
+                    required 
+                />
                 <AddButton type="submit">Dodaj</AddButton>
             </Form>
         </Container>
