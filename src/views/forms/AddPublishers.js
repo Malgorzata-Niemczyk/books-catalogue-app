474ee2fcfc4ac3e5120d4d065f8dbf189703a24b
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-03 20:57:23 +0200

    create the JSX template for the form to use it to add a new publisher

--- src/views/forms/AddPublishers.js
+++ src/views/forms/AddPublishers.js
@@ -1,6 +1,41 @@
+import { useContext, useState } from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import { PublishersContext } from '../../store/PublishersStore';
+
+import { Container } from "../../components/Home";
+import { Form, Label, Input, BackToListButton, ButtonsWrapper } from '../../styled-components/styled-form';
+import { AddButton } from '../../styled-components/styled-table';
+
+
 const AddPublishers = () => {
     return ( 
-        null
+        <Container>
+            <h2>Dodaj Nowe Wydawnictwo</h2>
+            <Form>
+                <Label htmlFor="lastName">Nazwa Wydawnictwa:</Label>
+                <Input 
+                    type="text" 
+                    // value={lastName}
+                    // onChange={event => setLastName(event.target.value)}
+                    placeholder="Wpisz nazwę wydawnictwa" 
+                    required 
+                />
+                <Label htmlFor="firstName">Rok Założenia:</Label>
+                <Input 
+                    type="text" 
+                    // value={firstName}
+                    // onChange={event => setFirstName(event.target.value)}
+                    placeholder="Wpisz rok założenia" 
+                    required 
+                />
+                <ButtonsWrapper>
+                    <AddButton type="submit">Dodaj</AddButton>
+                    <Link to="/wydawnictwa">
+                            <BackToListButton>Powrót do Listy</BackToListButton>
+                    </Link>
+                </ButtonsWrapper>
+            </Form>
+        </Container>
      );
 }
  
