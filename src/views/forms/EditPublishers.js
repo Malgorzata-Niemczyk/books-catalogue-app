c761e5ba81336147a177318d10ac015c5a689e5d
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-03 23:18:56 +0200

    set the content of the form for editing publisher's data and set the method for updating the data when submitting the form

--- src/views/forms/EditPublishers.js
+++ src/views/forms/EditPublishers.js
@@ -1,6 +1,86 @@
+import { Container } from "../../components/Home";
+import { Form, Label, Input } from '../../styled-components/styled-form';
+import { SaveButton, BackToListButton, ButtonsWrapper } from '../../styled-components/styled-form';
+
+import { Link, useParams, useHistory } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useContext, useState } from 'react';
+import { PublishersContext } from '../../store/PublishersStore';
+
 const EditPublishers = () => {
+    const history = useHistory();
+    const { id } = useParams();
+    const [name, setName] = useState('');
+    const [establishmentYear, setEstablishmentYear] = useState('');
+    const [state, dispatch] = useContext(PublishersContext);
+
+    useEffect(() => {
+        const fetchPublisherDetails = async () => {
+            await fetch(`http://139.162.147.107:3493/publishers/${id}`)
+                .then(res => res.json())
+                .then(data => {
+                    setName(data.name);
+                    setEstablishmentYear(data.establishmentYear);
+            })
+        };
+
+        fetchPublisherDetails()
+    }, [id]);
+
+    const editPublisherDetails = async (editedPublisher) => {
+        await fetch(`http://139.162.147.107:3493/publishers/${id}`, {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(editedPublisher)
+        }).then(() => {
+            console.log('New publisher added');
+            dispatch({ type: 'ADD_PUBLISHERS', payload: editedPublisher});
+            history.push('/wydawnictwa');
+        }).catch(error => console.log(error))
+    }
+
+    const handleSave = (event) => {
+        event.preventDefault();
+
+        let editedPublisher = {
+            name,
+            establishmentYear
+        };
+
+        editPublisherDetails(editedPublisher);
+
+        setName('');
+        setEstablishmentYear('');
+    }
+
     return ( 
-        null
+        <Container>
+            <h2>Edytuj Dane Wydawnictwa</h2>
+            <Form onSubmit={handleSave}>
+                <Label htmlFor="lastName">Nazwa Wydawnictwa:</Label>
+                <Input 
+                    type="text"
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                    placeholder="Wpisz nową nazwę wydawnictwa" 
+                    required 
+                />
+                <Label htmlFor="firstName">Rok Założenia:</Label>
+                <Input 
+                    type="number"
+                    value={establishmentYear}
+                    onChange={event => setEstablishmentYear(event.target.value)}
+                    placeholder="Wpisz nowy rok założenia" 
+                    required 
+                />
+                <ButtonsWrapper>
+                    <SaveButton type="submit">Zapisz</SaveButton>
+                    <Link to="/wydawnictwa">
+                        <BackToListButton>Powrót do Listy</BackToListButton>
+                    </Link>
+                </ButtonsWrapper>
+            </Form>
+        </Container>
      );
 }
  
