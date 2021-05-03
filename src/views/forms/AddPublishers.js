b2287df9d12a1ba436e2774249faf32656d69bc8
Malgorzata Niemczyk <m.niemczyk86@gmail.com>
2021-05-03 21:32:09 +0200

    set the post request to add a new publisher upon submitting a form

--- src/views/forms/AddPublishers.js
+++ src/views/forms/AddPublishers.js
@@ -8,23 +8,51 @@ import { AddButton } from '../../styled-components/styled-table';
 
 
 const AddPublishers = () => {
+    const history = useHistory();
+    const [name, setName] = useState('');
+    const [establishmentYear, setEstablishmentYear] = useState();
+    const [state, dispatch] = useContext(PublishersContext);
+    const url = 'http://139.162.147.107:3493/publishers';
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        let newPublisher = {
+            name,
+            establishmentYear
+        };
+
+        await fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(newPublisher)
+        }).then(() => {
+            console.log('New publisher added');
+            dispatch({ type: 'ADD_PUBLISHER', payload: newPublisher});
+            history.push('/wydawnictwa');
+        })
+        
+        setName('');
+        setEstablishmentYear('');
+    };
+
     return ( 
         <Container>
             <h2>Dodaj Nowe Wydawnictwo</h2>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Label htmlFor="lastName">Nazwa Wydawnictwa:</Label>
                 <Input 
                     type="text" 
-                    // value={lastName}
-                    // onChange={event => setLastName(event.target.value)}
+                    value={name}
+                    onChange={event => setName(event.target.value)}
                     placeholder="Wpisz nazwę wydawnictwa" 
                     required 
                 />
                 <Label htmlFor="firstName">Rok Założenia:</Label>
                 <Input 
-                    type="text" 
-                    // value={firstName}
-                    // onChange={event => setFirstName(event.target.value)}
+                    type="number"
+                    value={establishmentYear}
+                    onChange={event => setEstablishmentYear(event.target.value)}
                     placeholder="Wpisz rok założenia" 
                     required 
                 />
